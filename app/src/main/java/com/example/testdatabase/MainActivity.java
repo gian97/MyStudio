352445082301ffd957ad9db0d970a89118446604
zuna9 <zuna96@hotmail.it>
2019-10-01 21:47:35 +0200

    1/10 21:47 cambiato nome funzione

--- app/src/main/java/com/example/testdatabase/MainActivity.java
+++ app/src/main/java/com/example/testdatabase/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                     if(s2.equals(s3)){
                         Boolean chkemail = db.chkemail(s1);
                         if(chkemail == true){
-                            Boolean insert = db.insert(s1, s2);
+                            Boolean insert = db.insertdata(s1, s2);
                             if(insert==true){
                                 Toast.makeText(getApplicationContext(), "Registrazione avvenuta", Toast.LENGTH_SHORT).show();
                                 Intent i = new Intent(MainActivity.this, Home.class);
