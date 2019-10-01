bb2b33b06c6f3fcdd09cbfd91571bb232e32702e
zuna9 <zuna96@hotmail.it>
2019-10-01 23:44:02 +0200

    1/10 23:43  aggiunto popup-menu

--- app/src/main/java/com/example/testdatabase/Prenotazione.java
+++ app/src/main/java/com/example/testdatabase/Prenotazione.java
@@ -3,12 +3,38 @@ package com.example.testdatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-public class Prenotazione extends AppCompatActivity {
+public class Prenotazione extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prenotazione);
     }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.pp:
+                Toast.makeText(this, "Scelto primo piano", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.pt:
+                Toast.makeText(this, "Scelto piano terra", Toast.LENGTH_SHORT).show();
+                return true;
+
+                default: return false;
+        }
+    }
 }
+
