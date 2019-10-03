5bc5ba6750b17aeb40f072f16a12812749651f91
zuna9 <zuna96@hotmail.it>
2019-10-03 15:17:17 +0200

    3/10 15.16

--- app/src/main/java/com/example/testdatabase/Login.java
+++ app/src/main/java/com/example/testdatabase/Login.java
@@ -3,6 +3,10 @@ package com.example.testdatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,4 +48,6 @@ public class Login extends AppCompatActivity {
         Intent ac = new Intent(this, MainActivity.class);
         startActivity(ac);
     }
+
+
 }
